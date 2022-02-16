bb0b5fd4ea586fa9b36f04c518e53a6c5983fe90
askvart <tur1amant@gmail.com>
2022-02-16 23:57:08 +0300

    refactoring handler.py

--- handler.py
+++ handler.py
@@ -74,8 +74,10 @@ async def main():
     work_queue = asyncio.Queue()
     while True:
         correct_time = datetime.datetime.now().replace(second=0, microsecond=0)
-        q = session.query(Task).filter(Task.date_on == correct_time).\
-            filter(Task.task_done == 'False').all()
+        q = session.query(Task)\
+            .filter(Task.date_on == correct_time)\
+            .filter(Task.task_done == 'False')\
+            .all()
         for work in q:
             work_queue.put_nowait(work)
         await asyncio.gather(asyncio.create_task(worker("-One-", work_queue)),
